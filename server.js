e7466f420536709eccb06f0312eddb6adf1e65d3
sharkbyte <sgquin@gmail.com>
2022-04-26 14:24:28 -0700

    added routes for notes page

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
 
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')))
+
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
+
 
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
