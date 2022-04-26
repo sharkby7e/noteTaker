5e462e726136772df7b4fb22a4c396067d1a88e3
sharkbyte <sgquin@gmail.com>
2022-04-25 17:48:13 -0700

    testing public directory access

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
+const app = express()
+const PORT = 3000
 
-const PORT = 3000;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
 
 
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
 
