5ee7871922bb5a7545c3d0e5f1798aa0b86633ea
sharkbyte <sgquin@gmail.com>
2022-04-25 21:24:37 -0700

    trying to rebuild on heroku

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path')
 
 const app = express()
 const PORT = process.env.port || 3001;
-// const PORT = 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
