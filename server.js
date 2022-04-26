c371a167ea4d0923d9d98687a6e538de4ebe477e
sharkbyte <sgquin@gmail.com>
2022-04-25 17:41:17 -0700

    public now available

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require('path');
 const app = express();
 
 const PORT = 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
