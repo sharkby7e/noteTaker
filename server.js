631875577ff1dcb4a0415b340729edcf48954df4
sharkbyte <sgquin@gmail.com>
2022-04-25 21:18:20 -0700

    process.env.port instead of hard coding 3000

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.port || 3001;
+// const PORT = 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
