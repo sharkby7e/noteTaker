50439815e771a02d136030dceab8ec88e18f9e5b
sharkbyte <sgquin@gmail.com>
2022-04-25 17:37:46 -0700

    added server.js, with wildcard route

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
